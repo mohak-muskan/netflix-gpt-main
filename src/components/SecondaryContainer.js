d874c6dd99775bcdb8804557defef8b94b24551c
Mohak Muskan <mohakmuskan33@gmail.com>
2024-03-19 10:54:01 +0530

    UI bug fixes

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -7,14 +7,11 @@ const SecondaryContainer = () => {
   return (
     movies.nowPlayingMovies && (
       <div className="bg-black">
-        <div className=" mt-0 md:-mt-52 pl-4 md:pl-12 relative z-20">
+        <div className=" mt-0 md:-mt-40 pl-4 md:pl-12 relative z-20">
           <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
           <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
           <MovieList title={"Popular"} movies={movies.popularMovies} />
-          <MovieList
-            title={"Upcoming Movies"}
-            movies={movies.nowPlayingMovies}
-          />
+          <MovieList title={"Upcoming Movies"} movies={movies.nowPlayingMovies}/>
           <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
         </div>
       </div>
